bfa28d13922f9ba27d11e723b4eaa37aafeda646
GinoValenti <ginorvalenti@gmail.com>
2022-11-01 20:33:50 -0300

    doing the data base

---
+++ src/components/shows.js
@@ -0,0 +1,59 @@
+let shows = [ 
+    {
+        'id': 'it1',
+        'hotelId': 'ho1' ,
+        'name': 'Linkin Park',
+        'photo': './img/showsimg/show-hilton-bsas.jpg',
+        'price': '100' ,
+        'date': '2022-08-12' ,
+        'userId': 'user1',
+    },
+    {
+        'id': 'it2',
+        'hotelId': 'ho2' ,
+        'name': 'Pirates of the Caribean show',
+        'photo': './img/showsimg/show-westlin-hotel.jpg',
+        'price': '100' ,
+        'date': '2023-12-07' ,
+        'userId': 'user1',
+    },
+    {
+        'id': 'it3',
+        'hotelId': 'ho3' ,
+        'name': 'Comicon',
+        'photo': './img/showsimg/show-hotel-mexico.jpg',
+        'price': '100' ,
+        'date': '2023-12-05' ,
+        'userId': 'user1',
+    },
+
+{
+    "id": "it4",
+    "hotelId":"ho4",
+    "name":"Fashion show",
+    "photo":"./img/showsimg/showtheritz.jpg",
+    "price":"1000",
+    "date":"2023-01-12",
+    "userId": "user1"
+},
+{
+    "id": "it5",
+    "hotelId":"ho5",
+    "name":"Metallica!",
+    "photo":"./img/showsimg/metallica1.webp",
+    "price":"600",
+    "date":"2023-05-09",
+    "userId": "user2"
+},
+{
+    "id": "it6",
+    "hotelId":"ho6",
+    "name":"Coldplay",
+    "photo":"./img/showsimg/coldplay.jpg",
+    "price":"800",
+    "date":"2022-12-20",
+    "userId": "user3"
+},
+
+]
+export default shows;
