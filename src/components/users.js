bb7cbde2155665761c8857ce2b6426fa881b65c4
GastonZ <gastonzappullaryu@gmail.com>
2022-11-01 19:22:59 -0300

    Array of 12 cities + array of 6 cities and activities

---
+++ src/components/users.js
@@ -0,0 +1,49 @@
+let users = [
+    {
+        'id': 'user1',
+        'name': 'Gaston',
+        'lastName': 'Zappulla',
+        'age': 23,
+        'email': 'gastonzappullary@gmail.com',
+        'password': 'Lucybestwaifu123',
+        'code': '1999',
+        'verified': true,
+        'logged': true
+    },
+    {
+        'id': 'user2',
+        'name': 'Gino',
+        'lastName': 'Valenti',
+        'age': 19,
+        'email': 'ginorvalenti@gmail.com',
+        'password': 'Cyberpunk2077',
+        'code': '2003',
+        'verified': true,
+        'logged': true
+    },
+    {
+        'id': 'user3',
+        'name': 'Regina',
+        'lastName': 'Diaz',
+        'age': 23,
+        'email': 'reginadiaz@gmail.com',
+        'password': 'Lovemybf123',
+        'code': '2000',
+        'verified': true,
+        'logged': true
+    },
+    {
+        'id': 'user4',
+        'name': 'David',
+        'lastName': 'Martinez',
+        'age': 18,
+        'email': 'davidcyber@gmail.com',
+        'password': 'killadamsmasher123',
+        'code': '2077',
+        'verified': true,
+        'logged': true
+    }
+
+]
+
+export default users;
