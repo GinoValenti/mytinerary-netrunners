bc481e7f31370e848da5dcc3b6c20819693cefec
GinoValenti <ginorvalenti@gmail.com>
2022-11-02 21:57:04 -0300

    doing component AutoToTop,Footer,Banner,Cards and Carousel

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import './App.css';
+import "./components/carousel/Carousel"
+import "./components/carousel/carousel.css"
+import "./components/cardsopinion/Cards"
+import "./components/cardsopinion/cards.css"
+import Footer from "./components/footer/Footer"
+import "./components/footer/footer.css"
+import Cards from './components/cardsopinion/Cards';
+import Banner from './components/banner/Banner';
+import "./components/banner/banner.css";
+import Carousel from './components/carousel/Carousel';
+import AutoToTop from './components/AutoToTop/AutoToTop';
+import "./components/AutoToTop/AutoToTop.css"
+
+
 
 
 function App() {
-  console.log(hotels);
+
+
   return (
- <div></div>
+
+<body> 
+<Banner></Banner>
+<Carousel></Carousel>
+  <p className='opinion'>Some of the options most chosen by the public</p>
+  <Cards></Cards>
+  <AutoToTop></AutoToTop>
+  <Footer></Footer>
+</body>
   );
 }
 
---
+++ src/components/carousel/Carousel.jsx
@@ -0,0 +1,65 @@
+import React, { useState,useEffect } from 'react'
+import "../carousel/carousel.css"
+import carrusel from '../carrusel'
+import FotoCarrusel from '../FotoCarrusel/FotoCarrusel'
+
+function Carousel() {
+
+  let [numero, setNumero] = useState(0)
+  let [id, setId] = useState(0)
+
+  useEffect(
+    ()=>{  
+      let idInterval = setInterval(
+        //primer parametro la funcion que se va a ejecutar en cada intervalo de tiempo
+        ()=> {
+            next()
+            console.log('pasaron 5 segundos')
+        },
+        //segundo parametro es el intervalo en milisengudos
+        5000
+        //retorna un id asociado al intervalo (que es un numero)
+        //acepta una funcion que resetea el intervalo/contador con ese id
+      )
+      setId(idInterval)
+      return clearInterval(id)
+      // eslint-disable-next-line
+    },[numero])
+
+  function next() {
+
+    if (numero<carrusel.length-4){
+      setNumero(numero+4)
+    } else {
+      setNumero(0)
+    }
+  }
+
+  function prev() {
+    if (numero>3) {
+      setNumero(numero-4)
+    } else {
+      setNumero(carrusel.length-4)
+    }
+  }
+ 
+
+  return (
+    <>
+    <div className='carousel'>
+      <FotoCarrusel foto={carrusel[numero].foto} ></FotoCarrusel>
+      <FotoCarrusel foto={carrusel[numero+1].foto} ></FotoCarrusel>
+      <FotoCarrusel foto={carrusel[numero+2].foto} ></FotoCarrusel>
+      <FotoCarrusel foto={carrusel[numero+3].foto} ></FotoCarrusel>
+    </div>
+    <div className='btn-container'>
+        <button onClick={prev}><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAK1JREFUSEvtlNsNgzAQBIcO0gkpgZSQSmgh6SgdQAnQSUpAJ4F0imLfGsMf/rZmbtePhpNXczKfSxA2XFLRDeiBd0h1G1SBwQfgvgpeqkQRePgMdMD3KEEV3IbIJaiG5wSHwFMCD1erTg77ryITjEBbQk/VnToDL5mAR8nN8YNFh7wl2S2J3kF1kkhgaaskiuBX8gQ+6gVQBZvEvgkZHr1kdcjsvpIEu4SXIKxtAb+eIBmkl/3TAAAAAElFTkSuQmCC"/></button>
+        <button onClick={next}><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAKZJREFUSEvtlMENwjAQBMcd0AmUkHRAR6EEOqGEUAJ0QgdE90NRZN8uOC/8tmbkvfUVOp/Smc9f0EzYiegCXIFXkw7yDAI+AQ9gzEjUFxyAO3DMSlRBpCJJHIEkcQVpSU3wzrTk487m4H8peALDulnfRjQDJ2ATHq9zBdGkJtwVpOGOQII7gjNwq2W+bp4zg5DEuuiy7MSv4bcoLXIiSsOdIUvwXQQLDaAoGREMQ18AAAAASUVORK5CYII="/></button>
+    </div>
+    
+    </>
+
+  )
+}
+
+export default Carousel
