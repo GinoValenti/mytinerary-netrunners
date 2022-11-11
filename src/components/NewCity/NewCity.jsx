e24a469ab0ca9408df3c7762eaa4191148b251e4
GastonZ <gastonzappullaryu@gmail.com>
2022-11-11 11:39:31 -0300

    MT-01-18

--- src/components/NewCity/NewCity.jsx
+++ src/components/NewCity/NewCity.jsx
@@ -1,22 +1,25 @@
+import axios from 'axios';
 import React, { useState } from 'react'
 import './newcity.css'
+
 
 function NewCity() {
 
 
-  const [name, setName] = useState('');
+  const [title, setTitle] = useState('');
   const [continent,  setContinent] = useState('')
-  const [photo, setPhoto] = useState('');
+  const [image, setImage] = useState('');
   const [population, setPopulation] = useState('');
-  const [adminId, setAdminId] = useState('')
+  const [userId, setUserId] = useState('')
 
 
-    const submit = () => {
-        if (name === "" || continent === "" || photo === "" || population === "" || adminId === "") {
+    const submit = async () => {
+        if (title === "" || continent === "" || image === "" || population === "" || userId === "") {
             alert("Complete all fields");
         } else {
-            let newCity = JSON.stringify({name,continent,photo,population, adminId})
+            let newCity = {title,continent,image,population, userId}
             console.log(newCity)
+            axios.post(('http://localhost:8000/api/cities'), newCity)
         }
     };
 
@@ -25,10 +28,10 @@ function NewCity() {
     <div className='new-container'>
       <div className='form-container'>
       <input htmlFor='title' className='new-input' name='title' type="text"
-        onChange={(e) => setName(e.target.value)}
+        onChange={(e) => setTitle(e.target.value)}
         placeholder='Enter city name' />
         <input htmlFor='image' className='new-input' name='image' type="text"
-        onChange={(e) => setPhoto(e.target.value)}
+        onChange={(e) => setImage(e.target.value)}
         placeholder='Enter city photo' />
         <input htmlFor='continent' className='new-input' name='continent' type="text"
         onChange={(e) => setContinent(e.target.value)}
@@ -37,7 +40,7 @@ function NewCity() {
         onChange={(e) => setPopulation(Number(e.target.value))}
         placeholder='Enter city population' />
         <input htmlFor='userId' className='new-input' name='userId' type="text"
-        onChange={(e) => setAdminId(e.target.value)}
+        onChange={(e) => setUserId(e.target.value)}
         placeholder='Enter admin id' />        
         <button
         className='save-new-button' onClick={submit}>
