7bff6a0ba8a397ecc3c18a27aea5893000cfb7c5
GinoValenti <ginorvalenti@gmail.com>
2022-11-12 19:27:16 -0300

    MT-02-24 FINISHED

--- src/components/HotelCards/HotelCards.jsx
+++ src/components/HotelCards/HotelCards.jsx
@@ -5,35 +5,47 @@ import "./searchbar.css"
 import axios from "axios"
 import { Link, useParams } from 'react-router-dom';
 import { useEffect } from 'react'
+import { BASE_URL } from '../../api/url'
 
 
 
 export default function HotelCards() {
   let [filter,setFilter]=useState([])
   let [searched,setSearched]=useState([])
+  let [select, setSelect]=useState([])
   const [hotels, setHotels] = useState([]) 
   
   useEffect(()=>{
-    axios.get("http://localhost:8000/api/hotels")
+    axios.get(`${BASE_URL}/hotels`)
     .then (response =>setHotels(response.data.allhotels))
     
     
   }, [])
   
   function listen(value){
-        
-   
+    
+      if(value.target.value === "asc"){
+        setSelect("&order="+value.target.value)
+      }if(value.target.value === "desc") {
+        setSelect("&order="+value.target.value)
+      }
+
+    console.log(select)
 
     if(value.target.type==="text"){
         setSearched(value.target.value)
+        console.log(setSearched)
     }
-    
+    if(value.target.type==="text"){
+      setSearched(value.target.value)
+      console.log(setSearched)
+  }
    
 }
 useEffect(()=>{
-  axios.get(`http://localhost:8000/api/hotels?name=${searched}`)
+  axios.get(`${BASE_URL}/hotels?name=${searched}${select}`)
   .then(response=>setFilter(response.data.allhotels))
-},[searched])
+},[searched, select])
 
 console.log(filter)
 
@@ -48,13 +60,13 @@ console.log(filter)
 </form>
 </main>
 
-<SelectHotels ></SelectHotels>
+<div className="filter-area">
+            <select onChange={listen} className="select" name="isAvailable">
+                <option value="desc">Capacidad Ascendendente</option>
+                <option value="asc">Capacidad Descendiente</option>
+            </select>
+        </div>
          
-      
-      
-    
-
-  
     <div className='containerCardsHotel'> 
     {filter.map((x)=>{
       return(
